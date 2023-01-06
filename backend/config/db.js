08e3e7b476058fbcc85c9a423450a7601c16ada1
Adriano <adriano_alves_2008@hotmail.com>
2023-01-06 14:58:16 -0300

    conexão com o banco

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,1 +1,23 @@
 const mongoose = require('mongoose')
+
+const dbUser = process.env.DB_USER
+const dbPassword = process.env.DB_PASS
+
+const conn = async () => {
+  try {
+    mongoose.set('strictQuery', true)
+    const dbConn = await mongoose.connect(
+      `mongodb+srv://${dbUser}:${dbPassword}@cluster0.55x5xup.mongodb.net/?retryWrites=true&w=majority`
+    )
+
+    console.log('Conectou ao banco')
+
+    return dbConn
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+conn()
+
+module.exports = conn
