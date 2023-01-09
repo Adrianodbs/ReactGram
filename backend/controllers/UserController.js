a29eb034a2d1d51dfb731894015936186e30bf81
Adriano <adriano_alves_2008@hotmail.com>
2023-01-09 10:13:07 -0300

    iniciando validações

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -1,6 +1,6 @@
 const User = require('../models/User.js')
 
-const bcrypt = require('bcrypt.js')
+const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
 
 const jwtSecret = process.env.JWT_SECRET
