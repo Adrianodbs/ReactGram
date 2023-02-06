d5dfc3dde18d463075e176a1d25007a00ffe2671
Adriano <adriano_alves_2008@hotmail.com>
2023-02-06 16:11:41 -0300

    corrigindo bug

--- backend/middlewares/userValidations.js
+++ backend/middlewares/userValidations.js
@@ -9,20 +9,20 @@ const userCreateValidation = () => {
       .withMessage('O nome precisa ter no mínimo 3 caracteres.'),
     body('email')
       .isString()
-      .withMessage('O email é obrigatório.')
+      .withMessage('O e-mail é obrigatório.')
       .isEmail()
-      .withMessage('Insira um e-mail valido.'),
+      .withMessage('Insira um e-mail válido'),
     body('password')
       .isString()
       .withMessage('A senha é obrigatória.')
       .isLength({ min: 5 })
-      .withMessage('A senha precisa ter no mínimo 5 caracteres.'),
+      .withMessage('A senha precisa de no mínimo 5 caracteres.'),
     body('confirmPassword')
       .isString()
       .withMessage('A confirmação de senha é obrigatória.')
       .custom((value, { req }) => {
         if (value != req.body.password) {
-          throw new Error('As senhas não são iguais')
+          throw new Error('As senhas não são iguais.')
         }
         return true
       })
@@ -33,9 +33,9 @@ const loginValidation = () => {
   return [
     body('email')
       .isString()
-      .withMessage('O e-mail é obrigatório')
+      .withMessage('O e-mail é obrigatório.')
       .isEmail()
-      .withMessage('Insira um e-mail valido.'),
+      .withMessage('Insira um e-mail válido'),
     body('password').isString().withMessage('A senha é obrigatória.')
   ]
 }
@@ -49,7 +49,7 @@ const userUpdateValidation = () => {
     body('password')
       .optional()
       .isLength({ min: 5 })
-      .withMessage('A senha precisa ter no mínimo 5 caracteres.')
+      .withMessage('A senha precisa de no mínimo 5 caracteres.')
   ]
 }
 
