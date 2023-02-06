bfe23d7fc105b4b3012d9a7af2de4ff65fd9c8b8
Adriano <adriano_alves_2008@hotmail.com>
2023-02-06 11:10:57 -0300

    conectando o formulário ao reducer

--- frontend/src/pages/Auth/Register.js
+++ frontend/src/pages/Auth/Register.js
@@ -4,12 +4,20 @@ import { Link } from 'react-router-dom'
 
 //Hooks
 import { useState, useEffect } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+
+//redux
+import { register, reset } from '../../slices/authSlice'
 
 const Register = () => {
   const [name, setName] = useState('')
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
   const [confirmPassword, setConfirmPassword] = useState('')
+
+  const dispatch = useDispatch()
+
+  const { loading, error } = useSelector(state => state.auth)
 
   const handleSubmit = e => {
     e.preventDefault()
@@ -22,7 +30,15 @@ const Register = () => {
     }
 
     console.log(user)
+
+    dispatch(register(user))
   }
+
+  // Clean all auth states
+  useEffect(() => {
+    dispatch(reset())
+  }, [dispatch])
+
   return (
     <div id="register">
       <h2>ReactGram</h2>
@@ -32,25 +48,25 @@ const Register = () => {
           type="text"
           placeholder="Nome"
           onChange={e => setName(e.target.value)}
-          value={name || ''}
+          value={name}
         />
         <input
           type="email"
           placeholder="E-mail"
           onChange={e => setEmail(e.target.value)}
-          value={email || ''}
+          value={email}
         />
         <input
           type="password"
           placeholder="Senha"
           onChange={e => setPassword(e.target.value)}
-          value={password || ''}
+          value={password}
         />
         <input
           type="password"
           placeholder="Confirme a senha"
           onChange={e => setConfirmPassword(e.target.value)}
-          value={confirmPassword || ''}
+          value={confirmPassword}
         />
         <input type="submit" value="cadastrar" />
       </form>
